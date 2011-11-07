ed5f8eeb0e5b150adb527e1d584945a662ccdcb0
Patricia Pons <patriciapons89@gmail.com>
2011-11-07 12:19:02 +0100

    ofertas modificado, modelo modificado

--- static/base.js
+++ static/base.js
@@ -8,5 +8,29 @@ $('.close').live('click', function() {
 
 $(document).ready(function() {
         $(".sortable").tablesorter();
+        
+        if(window.location.pathname == "/profile/copyoffers") {
+            var buttons = $("#exchange-buttons");
+            $("#offersTable input[type=radio]").live('click', function () {
+                $(buttons).children().removeClass("disabled");
+            });       
+        }
+        
+        if(window.location.pathname == "/profile/appliantcopies") {
+            var user = (RegExp("appliant=(.*)").exec(window.location.href.slice(window.location.href.indexOf('?') + 1).split('&')[1]))[1];
+            var rows = $(".appliant-column-name");
+            
+            for (var i = 0; i < rows.length; i++) {
+                if ($(rows[i]).text() == user) {
+                    $(rows[i]).parent().children().first().children().first().attr("checked", "");
+                }
+            }
+            $("#exchange-buttons").children().removeClass("disabled");
+            
+            var buttons = $("#exchange-buttons2");
+            $("#appliantCopiesTable input[type=radio]").live('click', function () {
+                $(buttons).children().removeClass("disabled");
+            });  
+        }
 });
 
